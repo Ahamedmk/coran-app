0af3cfe52e5520780f3acd8432da14df0eb6dbf4
Ahamedmk <amkavavo1@gmail.com>
2025-10-31 08:22:08 +0100

    Add environment variables

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,7 @@
 // src/config/supabase.js
 
-const SUPABASE_URL = 'https://pkokrgpxcqaxmhzgggev.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrb2tyZ3B4Y3FheG1oemdnZ2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0OTg4ODksImV4cCI6MjA3NzA3NDg4OX0.yaqn0EcSQTLiCYDTqEq5UtJnkSzN5Zh9t3hkTXozGQM';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://pkokrgpxcqaxmhzgggev.supabase.co';
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrb2tyZ3B4Y3FheG1oemdnZ2V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0OTg4ODksImV4cCI6MjA3NzA3NDg4OX0.yaqn0EcSQTLiCYDTqEq5UtJnkSzN5Zh9t3hkTXozGQM';
 
 export const supabase = {
   from: (table) => ({
